c3f70c6448e4f67fffe6ab54e3ae2b97600a8890
Steffan Andrews <stef@orchetect.com>
2026-08-05 15:57:15 -0700

    Fixed syntax error in SDK verify script

--- scripts/sdk-verify.sh
+++ scripts/sdk-verify.sh
@@ -11,7 +11,7 @@
 # (none)
 
 # Get SDK path.
-SDK_INFO=$(swift sdk configure --show-configuration "${SDK_ID} "${TARGET_TRIPLE}"")
+SDK_INFO=$(swift sdk configure --show-configuration "${SDK_ID}" "${TARGET_TRIPLE}")
 SDK_PATH_REGEX="m/sdkRootPath\:\s(\S*\/WASI.sdk)/g"
 SDK_PATH=$(echo "${SDK_INFO}" | perl -nle 'if ('$SDK_PATH_REGEX') { ($path) = ($1); print "${path}"; }')
 if [ -z $SDK_PATH ]; then echo "⛔️ Error: SDK Path not found."; exit 1; fi
